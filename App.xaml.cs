f044e829d18afa90202614f2c082f4e0db60f18c
mariuszkuswik <mariusz.kuswik1@gmail.com>
2022-09-09 12:04:23 +0200

    Utworzona baza + dodatkowe okno

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace cs_proj_ostateczny
+{
+    /// <summary>
+    /// Logika interakcji dla klasy App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Przystanki.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cs_proj_ostateczny
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Przytanki.xaml
+    /// </summary>
+    public partial class Przytanki : Window
+    {
+        public Przytanki()
+        {
+            
+        }
+    }
+}
