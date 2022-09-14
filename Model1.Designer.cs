7454cf189f3f4daa4073677cbc4590753ff72a02
mariusz.kuswik <mariusz.kuswik1@gmail.com>
2022-09-14 23:41:07 +0200

    Czyszczenie gita

--- Model1.Designer.cs
+++ Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Mariusz\source\repos\cs-proj-ostateczny\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Domin\Documents\Mariusz2\cs-proj-semestr\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
