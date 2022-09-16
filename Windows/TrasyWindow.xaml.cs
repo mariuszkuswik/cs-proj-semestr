220a9ec7608dc84d625826e4a30f2ac3fea02793
mariusz.kuswik <mariusz.kuswik1@gmail.com>
2022-09-16 17:35:42 +0200

    Dodanie wyświetlania przystanków na trasie

--- Windows/TrasyWindow.xaml.cs
+++ Windows/TrasyWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Data.Entity;
+using System.ComponentModel;
+using System.Collections.Specialized;
 
 namespace cs_proj_ostateczny
 {
@@ -23,37 +25,77 @@ namespace cs_proj_ostateczny
     {
         csprojEntities context = new csprojEntities();
         CollectionViewSource trasyViewSource;
+        CollectionViewSource przystankiNaTrasieViewSource;
+        CollectionViewSource przystankiViewSource;
+
         public TrasyWindow()
         {
             InitializeComponent();
         }
         private void NextCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
-
+            trasyViewSource.View.MoveCurrentToNext();
+            if (trasyViewSource.View.IsCurrentAfterLast)
+            {
+                trasyViewSource.View.MoveCurrentToPrevious();
+            }
         }
         private void PreviousCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
 
         }
-        private void DeleteCustomerCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        private void DeleteCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
 
         }
-        private void UpdateCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        private void SwitchCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
 
         }
-        private void AddCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        private void CommitCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
 
         }
+
+        
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            context.Przystanki.Load();
+            przystankiViewSource = ((CollectionViewSource)(this.FindResource("przystankiViewSource")));
+            przystankiViewSource.Source = context.Przystanki.Local;
+
+            context.Przystanki_na_trasie.Load();
+            przystankiNaTrasieViewSource = ((CollectionViewSource)(this.FindResource("przystankiNaTrasieViewSource")));
+            przystankiNaTrasieViewSource.Source = context.Przystanki_na_trasie.Local;
+
             context.Trasy.Load();
-            System.Windows.Data.CollectionViewSource trasyViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("trasyViewSource")));
-            // Załaduj dane poprzez ustawienie właściwości CollectionViewSource.Source:
+            trasyViewSource = ((CollectionViewSource)(this.FindResource("trasyViewSource")));
             trasyViewSource.Source = context.Trasy.Local;
+            
+
+            przystankiNaTrasieViewSource.Filter += new FilterEventHandler(filterByPrzystanekId);
+        }
+
+        private void filterByPrzystanekId(object sender, FilterEventArgs e)
+        {
+            var obj = e.Item as Przystanki_na_trasie;
+            var currentPrzystanek = trasyViewSource.View.CurrentItem as Trasy;
+            if (currentPrzystanek != null)
+            {
+                if (obj != null)
+                {
+                    if (obj.id_trasy == currentPrzystanek.id)
+                        e.Accepted = true;
+                    else
+                        e.Accepted = false;
+                }
+            }
+        }
+
+        private void trasyDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            przystankiNaTrasieViewSource.View.Refresh();
         }
     }
 }
